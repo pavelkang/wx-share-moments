8ce94d5274fb9c067fc1cb4f7cc7447eaae98412
Kai <sclingtt@gmail.com>
2014-08-08 10:51:43 +0800

    v0.0.1

--- wx-share-moments.js
+++ wx-share-moments.js
@@ -1,7 +1,25 @@
 angular.module('pavelkang.wx-share-moments', []).
 	directive('wxShareMoments', function(){
+		var link = function(scope, element, attrs) {
+			document.addEventListener('WeixinJSBridgeReady', function onBridgeReady(){
+				WeixinJSBridge.on('menu:share:timeline', function(argv){
+					WeixinJSBridge.invoke('shareTimeline', {
+						"img_url" :  attrs['img'],
+						"img_width" : attrs['img_width']?attrs['img_width']:120,
+						"img_height" : attrs['img_height']?attrs['img_height']:120,
+						"link" : attrs['link'],
+						"title" : attrs['title']
+					}, function(res) {
+						_report('send_msg', res.err_msg);
+					})
+				});
+			}, false);
+		};
 		return {
 			restrict : "AE",
-			template : "<p>Hello World</p>"
+			link : link,
+			scope : {
+
+			}
 		}
 	});
