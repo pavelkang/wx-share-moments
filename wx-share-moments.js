0b100cb9b8cb4f14133e15fc1cda1c1f0fd7ab59
Kai <sclingtt@gmail.com>
2014-08-08 13:02:38 +0800

    v0.0.6

--- wx-share-moments.js
+++ wx-share-moments.js
@@ -1,7 +1,6 @@
-angular.module('pavelkang.wx-share-moments', []).
-	directive('wxShareMoments', function(){
+angular.module('pavelkang.wx-share', [])
+	.directive('wxShareMoments', function(){
 		var link = function(scope, element, attrs) {
-			alert(attrs['link'])
 			document.addEventListener('WeixinJSBridgeReady', function onBridgeReady(){
 				WeixinJSBridge.on('menu:share:timeline', function(argv){
 					WeixinJSBridge.invoke('shareTimeline', {
@@ -24,4 +23,30 @@ angular.module('pavelkang.wx-share-moments', []).
 
 			}
 		}
-	});
+	})
+	.directive('wxShareFriends', function(){
+		var link = function(scope, element, attrs) {
+			document.addEventListener('WeixinJSBridgeReady', function onBridgeReady(){
+				WeixinJSBridge.on('menu:share:appmessage', function(argv){
+					WeixinJSBridge.invoke('sendAppMessage', {
+						"appid" : attrs["appid"] ? attrs["appid"] : "",
+						"img_url" :  attrs['img'] ? attrs['img'] : 'http://t12.baidu.com/it/u=2348010170,4054977331&fm=58',
+						"img_width" : "120",
+						"img_height" : "120",
+						"link" : attrs['link'] ? attrs['link'] : 'http://weixin.qq.com',
+						"desc" : attrs["desc"] ? attrs["desc"] : "No description",
+						"title" : attrs["title"] ? attrs["title"] : "Title"
+					}, function(res) {
+						_report('send_msg', res.err_msg);
+					});
+				});
+			}, false);
+		};
+		return {
+			restrict : "AE",
+			link : link,
+			scope : {
+
+			}
+		}
+	});	
