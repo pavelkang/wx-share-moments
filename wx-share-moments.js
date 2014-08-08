696de57a032bd5ac7033611234fc67f0dc101a4c
Kai <sclingtt@gmail.com>
2014-08-08 12:56:09 +0800

    v0.0.5

--- wx-share-moments.js
+++ wx-share-moments.js
@@ -5,7 +5,7 @@ angular.module('pavelkang.wx-share-moments', []).
 			document.addEventListener('WeixinJSBridgeReady', function onBridgeReady(){
 				WeixinJSBridge.on('menu:share:timeline', function(argv){
 					WeixinJSBridge.invoke('shareTimeline', {
-						"img_url" :  attrs['img'] ? attrs['img'] : 'http://t12.baidu.com/it/u=2348010170,4054977331&fm=58'
+						"img_url" :  attrs['img'] ? attrs['img'] : 'http://t12.baidu.com/it/u=2348010170,4054977331&fm=58',
 						"img_width" : "120",
 						"img_height" : "120",
 						"link" : attrs['link'] ? attrs['link'] : 'http://weixin.qq.com',
