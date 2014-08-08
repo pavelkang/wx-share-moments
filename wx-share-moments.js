0bb5883aa2b2257ce67429d8eee93b83b1f940f7
Kai <sclingtt@gmail.com>
2014-08-08 11:18:25 +0800

    v0.0.2

--- wx-share-moments.js
+++ wx-share-moments.js
@@ -5,11 +5,11 @@ angular.module('pavelkang.wx-share-moments', []).
 			document.addEventListener('WeixinJSBridgeReady', function onBridgeReady(){
 				WeixinJSBridge.on('menu:share:timeline', function(argv){
 					WeixinJSBridge.invoke('shareTimeline', {
-						"img_url" :  attrs['img'],
-						"img_width" : attrs['img_width']?attrs['img_width']:120,
-						"img_height" : attrs['img_height']?attrs['img_height']:120,
-						"link" : attrs['link'],
-						"title" : attrs['title']
+						"img_url" :  "http://kaikang.herokuapp.com/images/self.jpg",
+						"img_width" : "120",
+						"img_height" : "120",
+						"link" : "http://kaikang.herokuapp.com",
+						"title" : "This is a test"
 					}, function(res) {
 						_report('timeline', res.err_msg);
 					});
